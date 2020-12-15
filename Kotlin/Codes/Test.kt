9e68d639e024e31811ffb48d893ada77859aec62
Mohit <mohitrathor8@gmail.com>
2020-12-15 11:50:43 +0530

    Updates Kotlin

--- Kotlin/Codes/Test.kt
+++ Kotlin/Codes/Test.kt
@@ -2,4 +2,6 @@ fun main() {
     val letter: Char    // defining a variable 
    letter = 'A'        // Assigning a value to it 
    println(letter) 
+   println(letter::class.simpleName)
+   println(letter::class.qualifiedName)
 }
