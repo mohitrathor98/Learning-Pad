ae8010436532cf4e1ce6662e47ad7c67e70fd6b3
Mohit <mohitrathor8@gmail.com>
2020-12-17 15:30:34 +0530

    Kotlin Assignment

---
+++ Kotlin/Codes/Assignments/Assignment1.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    print("Two space seperated numbers:: ")
+    var (firstNum,secondNum) = readLine()!!.split(' ').map(String::toInt)
+
+    var highNum:Int
+    if(firstNum > secondNum){
+        highNum = firstNum
+    } else {
+        highNum = secondNum
+    }
+
+    while(true){
+        if ((highNum % firstNum == 0) && (highNum%secondNum == 0)){
+            println("LCM of "+ firstNum +" and "+ secondNum +" is "+highNum)
+            break
+        }
+        highNum++
+    }
+}
