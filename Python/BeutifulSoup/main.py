bf4605026bcd30cf76880d3ef866902b3dc8e5aa
Mohit <mohitrathor8@gmail.com>
2022-01-01 11:44:45 +0530

    parsing html text using soup

--- Python/BeutifulSoup/main.py
+++ Python/BeutifulSoup/main.py
@@ -1,5 +1,6 @@
 # Learning Beutiful soup
 # Documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from os import name
 from bs4 import BeautifulSoup
 
 with open("website.html", "r") as file:
@@ -21,3 +22,36 @@ print(soup.title.name) # name of tag
 print(soup.title.string) # returns content of html page
 
 print(soup.a) # first anchor tag
+
+# returns all of the tags which is provided(a,p,hr,etc)
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.getText()) # returns text of the tags
+    print(tag.get("href")) # returns tag attribute value
+    
+
+# find returns only one instance of tag
+# we can pass it's attribute value also to get hold of any
+# specific tag    
+# NOTE: Since class is a keyword in python
+#       to use class attribute we use class_
+heading = soup.find(name="h1", id="name") # here h1's attribute is id
+print(heading)
+
+
+# select_one: gives first matching item
+# select: fives all of the matching items
+
+# here it selects first anchor tag inside p tag
+# it works like css selectors
+company_url = soup.select_one(selector="p a") 
+print(company_url)
+
+# selects as per id name
+name = soup.select_one(selector="#name")
+print((name))
+
+# selects all the items with heading class
+headings = soup.select(selector=".heading")
+print(headings)
