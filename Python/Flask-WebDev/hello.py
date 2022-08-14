25aca46e6cb17d8042b918c9d2b5e9ef20264944
mohitrathor98 <mohitrathor8@gmail.com>
2022-08-14 16:27:13 +0530

    decorators and nested-functions

--- Python/Flask-WebDev/hello.py
+++ Python/Flask-WebDev/hello.py
@@ -1,11 +1,16 @@
 ####################################
-# Run using:                       #
+# Run using(if app.run not used):  #
 # flask --app hello run            #
 ####################################
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+# decorator
+@app.route("/") # when user goes to this path run below method
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run() # runs the file with python <file_name>.py
