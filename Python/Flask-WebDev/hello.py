83733e9d043763bc7008b01baf3ff807a94be27f
mohitrathor98 <mohitrathor8@gmail.com>
2023-04-16 14:19:48 +0530

    Adding how to handle undefined route entered user?

--- Python/Flask-WebDev/hello.py
+++ Python/Flask-WebDev/hello.py
@@ -7,14 +7,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# decorator function which is inside app function, which lives inside Flask class
-@app.route("/") # when user goes to this path run below method
+# decorator function which is inside app function,
+# which lives inside Flask class
+@app.route("/") 
+# when user goes to this path run below method
 def hello_world():
     return "<p>Hello, World!</p>"
+
 
 @app.route("/bye")
 def bye():
     return "Bye!!"
 
+
+# if user gives a route not defined in the application
+@app.errorhandler(404)
+def pageNotFound():
+    return "<h1>Sorry! This route is not handled."
+
+
 if __name__ == "__main__":
-    app.run() # runs the file with python <file_name>.py
+    app.run()
+    # runs the file with python <file_name>.py
